7a88079ab595827e146a72beef5502b1bf66a3dd
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-07-18 12:59:11 +0600

    Updated

--- Games/Rock Paper Scissors/RPC-source.c
+++ Games/Rock Paper Scissors/RPC-source.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 #ifdef _WIN32
 #include <Windows.h>
 #else
@@ -24,18 +25,20 @@ void winnerfunc(int robo,int hum)
 }
 int main()
 {
-    int robot,human,num,i,tp_human,tp_robot;
+    int robot,num,i,tp_human,tp_robot;
+    char human;
     tp_human=0;
     tp_robot=0;
     printf("Enter how many times you want to play:");
     scanf("%d",&num);
+    fflush(stdin);
     printf("---------------------Lets start!---------------------");
-    printf("\nNote: You will get penalty if you input invalid digits.\n");
+    printf("\nNote: You will get penalty if you input invalid characters.\n");
 
     for(i=0; i<num; i++)
-    {
-        printf("\nChoose your option-\n1.Rock\n2.Paper\n3.Scissor\n\nYour choice is-");
-        scanf("%d",&human);
+    {   fflush(stdin);
+        printf("\nChoose your option-\nr. Rock\np. Paper\ns. Scissor\n\nYour choice is-");
+        scanf("%c",&human);
         srand(time(NULL));
         robot=((rand()%3)+1);
         if(robot==1)
@@ -50,45 +53,46 @@ int main()
         {
             printf("Robot choosed Scissor\n");
         }
-        if(human==robot)
+
+            if((human=='r'&&robot==1)||(human=='p'&&robot==2)||(human=='s'&&robot==3))
         {
             printf("\nDraw!!!");
         }
-        else
-        {
-            if(human==1&&robot==2)
+           else if(human=='r'&&robot==2)
             {
                 printf("\nRobot Got 1 point");
                 tp_robot=tp_robot+1;
             }
-            else if(human==1&&robot==3)
+            else if(human=='r'&&robot==3)
             {
                 printf("\nYou Got 1 point");
                 tp_human=tp_human+1;
             }
-            else if(human==2&&robot==1)
+            else if(human=='p'&&robot==1)
             {
                 printf("\nYou Got 1 point");
                 tp_human=tp_human+1;
             }
-            else if(human==2&&robot==3)
+            else if(human=='p'&&robot==3)
             {
                 printf("\nRobot Got 1 point");
                 tp_robot=tp_robot+1;
             }
-            else if(human==3&&robot==1)
+            else if(human=='s'&&robot==1)
             {
                 printf("\nRobot Got 1 point");
                 tp_robot=tp_robot+1;
             }
-            else if(human==3&&robot==2)
+            else if(human=='s'&&robot==2)
             {
                 printf("\nYou Got 1 point");
                 tp_human=tp_human+1;
             }else{
             printf("\nInvalid Input!. As a penalty, Its gonna be counted as like draw.");
             }
-        }
+
+
+
         printf("\n--------------");
         printf("\n--------------");
         sleep(1);
